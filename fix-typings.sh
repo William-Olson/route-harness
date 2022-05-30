98a8053960db7354693d0d3e9acfd8f2c699701f
William Olson <willko747@gmail.com>
2022-05-30 15:05:47 -0500

    more fixes and cleanup

--- fix-typings.sh
+++ fix-typings.sh
@@ -2,14 +2,20 @@
 
 # run `./node_modules/.bin/tsc -p .` first to generate the declaration files
 
-# clear tmp file
-echo '' > ./route-harness.d.ts;
+# start dumping to tmp file
+cat ./build/Types.d.ts > ./route-harness.d.ts;
 
-cat ./build/Factory.d.ts ./build/RouteHarness.d.ts ./build/Types.d.ts > ./route-harness.d.ts
 echo -e '\n' >> ./route-harness.d.ts;
+cat ./build/Factory.d.ts >> ./route-harness.d.ts;
+
+# remove the 2nd and 3rd line with local imports
+echo -e '\n' >> ./route-harness.d.ts;
+cat ./build/RouteHarness.d.ts | sed '2d;3d' >> ./route-harness.d.ts;
+
 
 # remove the default export from Wrapper since everything is one file
-cat ./build/Wrapper.d.ts | sed '$d' >> ./route-harness.d.ts
+echo -e '\n' >> ./route-harness.d.ts;
+cat ./build/Wrapper.d.ts | sed '2d;$d' >> ./route-harness.d.ts
 
 # write to index.d.ts
 cat ./route-harness.d.ts > ./build/index.d.ts
